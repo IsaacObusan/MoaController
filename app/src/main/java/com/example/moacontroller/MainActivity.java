ecb0a855842631ad5ec5f517e298569da134a326
BurgerFox1 <jeloaguilar1@gmail.com>
2024-09-30 12:40:30 +0800

    commit all file

--- app/src/main/java/com/example/moacontroller/MainActivity.java
+++ app/src/main/java/com/example/moacontroller/MainActivity.java
@@ -1,24 +1,75 @@
 package com.example.moacontroller;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
+    private SwitchCompat switchCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        viewPager = findViewById(R.id.viewPager);
+        switchCompat = findViewById(R.id.Switch);
+
+
+        setupViewPager(viewPager);
+
+
+        switchCompat.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+
+                viewPager.setCurrentItem(1);
+            } else {
+
+                viewPager.setCurrentItem(0);
+            }
         });
+
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                switchCompat.setOnCheckedChangeListener(null);
+                if (position == 1) {
+                    switchCompat.setChecked(true);  // Manual fragment
+                } else {
+                    switchCompat.setChecked(false); // Automatic fragment
+                }
+                switchCompat.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                    if (isChecked) {
+                        viewPager.setCurrentItem(1);
+                    } else {
+                        viewPager.setCurrentItem(0);
+                    }
+                });
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // No action needed here
+            }
+        });
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        adapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        adapter.addFragment(new Manual());
+        adapter.addFragment(new Automatic());
+        viewPager.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/moacontroller/Manual.java
+++ app/src/main/java/com/example/moacontroller/Manual.java
@@ -1,22 +1,51 @@
 package com.example.moacontroller;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 public class Manual extends Fragment {
 
+    private SeekBar seekBar;
+    private ImageView thumbImage;
 
-
+    @SuppressLint("MissingInflatedId")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manual, container, false);
+        View view = inflater.inflate(R.layout.fragment_manual, container, false);
+
+        // Initialize SeekBar and ImageView
+        seekBar = view.findViewById(R.id.seekBar);
+        thumbImage = view.findViewById(R.id.floatingThumb);
+
+        // Set SeekBar listener to update thumb position
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Update the position of the floating thumb
+                int width = seekBar.getWidth();
+                float thumbOffset = (width / (float) seekBar.getMax()) * progress; // Calculate offset based on progress
+                thumbImage.setTranslationX(thumbOffset - (thumbImage.getWidth() / 2)); // Center the thumb
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+
+        return view;
     }
 }
