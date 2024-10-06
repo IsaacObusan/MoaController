1c5461206b12d8703621783deff8e7f89615e812
BurgerFox1 <jeloaguilar1@gmail.com>
2024-10-06 09:14:22 +0800

    Done, i'll create data output mamaya, then database store

--- app/src/main/java/com/example/moacontroller/Automatic.java
+++ app/src/main/java/com/example/moacontroller/Automatic.java
@@ -1,20 +1,39 @@
 package com.example.moacontroller;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
+import androidx.fragment.app.Fragment;
 
 public class Automatic extends Fragment {
+
+    private ImageView needle;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_automatic, container, false);
+        View view = inflater.inflate(R.layout.fragment_automatic, container, false);
+        needle = view.findViewById(R.id.needle);
+        float speed = 1;
+        rotateNeedle(speed);
+
+        return view;
+    }
+    private void rotateNeedle(float speed) {
+        float rotationAngle = getRotationAngleForSpeed(speed);
+        needle.setRotation(rotationAngle);
+    }
+
+
+    private float getRotationAngleForSpeed(float speed) {
+        switch ((int) speed) {
+            case 1: return -5f;
+            case 2: return 23f;
+            case 3: return 122f;
+            case 4: return 150f;
+            default: return 0f;
+        }
     }
 }
